0eada624700797bfaa9ede6831319f5fa72c6a49
Filipe Oliveira <hi@fmoliveira.dev>
2024-06-29 23:52:26 -0400

    Print basic messages list

--- morelit/app.py
+++ morelit/app.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import pandas as pd
 import notmuch2
+
+from datetime import datetime
 
 db = notmuch2.Database()
 filter = "unread tag:inbox"
 
+st.set_page_config(layout="wide")
 st.write("# morelit")
 st.write("unread messages:", db.count_messages(filter))
+
+headers = ["date", "from", "subject"]
+list = []
+
+for index, msg in zip(range(100), db.messages(filter, sort=db.SORT.NEWEST_FIRST)):
+  list.append([
+    datetime.fromtimestamp(msg.date),
+    msg.header("from"),
+    msg.header("subject"),
+  ])
+
+df = pd.DataFrame(list, columns=headers)
+st.write(df)
